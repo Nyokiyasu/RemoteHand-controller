8bc7d27ef77bceec6f55c2be948ee13ff612ec6f
Nyoki <hibari100206@gmail.com>
2017-07-01 11:23:58 +0900

    DipSWの読み取り関数を追加

--- program/src/main.c
+++ program/src/main.c
@@ -10,10 +10,46 @@
 
 
 #include "stm32f0xx.h"
-			
+
+
+void DipSW_init(void);
+uint8_t DipSW_read(void);
 
 int main(void)
 {
+	uint8_t Addr;
+	/*変数定義*/
 
-	for(;;);
+	DipSW_init();
+
+	while(1)
+	{
+			Addr = DipSW_read();
+	}
+
+}
+
+void DipSW_init(void)
+{
+	GPIO_InitTypeDef init_gpio;
+
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+
+	/*DipSW*/
+	GPIO_StructInit(&init_gpio);
+	init_gpio.GPIO_Pin = GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12;	//
+	init_gpio.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_Init(GPIOA,&init_gpio);
+
+	return;
+
+}
+
+uint8_t DipSW_read(void)
+{
+	uint8_t data=0;
+	data = GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_10) << 0;
+	data = data|GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_11) << 1;
+	data = data|GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_12) << 2;
+	return data;
 }
